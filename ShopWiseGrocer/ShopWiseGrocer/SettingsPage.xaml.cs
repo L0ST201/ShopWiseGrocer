36369c88269abd75450b4c170df0667ad9de7a33
Your Name <BenArmour72@gmail.com>
2023-08-03 20:53:34 -0400

    conflict fixes

--- ShopWiseGrocer/ShopWiseGrocer/SettingsPage.xaml.cs
+++ ShopWiseGrocer/ShopWiseGrocer/SettingsPage.xaml.cs
@@ -31,21 +31,60 @@ namespace ShopWiseGrocer
         public SettingsPage()
         {
             InitializeComponent();
-
-            // Set the BindingContext of the page to the current instance of the SettingsPage class
             this.BindingContext = this;
-        }
 
-        // This method is triggered when the switch is toggled
+            LanguagePicker.ItemsSource = new List<string>
+            {
+                "English",
+                "Spanish",
+                "French",
+            };
+
+            string currentLanguage = Preferences.Get("AppLanguage", "English");
+            LanguagePicker.SelectedIndex = LanguagePicker.ItemsSource.IndexOf(currentLanguage);
+
+            string fontSizeStr = Preferences.Get("AppFontSize", "16");
+            double currentFontSize;
+
+            if (!double.TryParse(fontSizeStr, out currentFontSize))
+            {
+                currentFontSize = 16;
+            }
+
+            FontSizeSlider.Value = currentFontSize;
+        }
         public void OnToggled(object sender, ToggledEventArgs e)
         {
             IsDarkTheme = e.Value;
         }
 
+        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                string selectedLanguage = (string)picker.ItemsSource[selectedIndex];
+
+                HandleLanguageChange(selectedLanguage);
+            }
+        }
+
+        void HandleLanguageChange(string language)
+        {
+            Preferences.Set("AppLanguage", language);
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Preferences.Set("AppFontSize", e.NewValue.ToString());
+        }
+
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            OnPropertyChanged(nameof(ThemeModeText)); // Notify that the ThemeModeText has changed
+            OnPropertyChanged(nameof(ThemeModeText));
         }
     }
 }
