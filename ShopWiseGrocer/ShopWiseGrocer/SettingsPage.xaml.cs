2c2070d7bce362953366705e055290caa55f2a97
burdhasp-uc <113551683+burdhasp-uc@users.noreply.github.com>
2023-08-03 20:15:53 -0400

    Created more settings

--- ShopWiseGrocer/ShopWiseGrocer/SettingsPage.xaml.cs
+++ ShopWiseGrocer/ShopWiseGrocer/SettingsPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace ShopWiseGrocer
 {
@@ -14,8 +15,42 @@ namespace ShopWiseGrocer
         public SettingsPage()
         {
             InitializeComponent();
+
+            LanguagePicker.ItemsSource = new List<string>
+        {
+            "English",
+            "Spanish",
+            "French",
+        };
+
+            string currentLanguage = Preferences.Get("AppLanguage", "English");
+            LanguagePicker.SelectedIndex = LanguagePicker.ItemsSource.IndexOf(currentLanguage);
+
+            double currentFontSize = Preferences.Get("AppFontSize", 16);
+            FontSizeSlider.Value = currentFontSize;
         }
 
+        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
 
+            if (selectedIndex != -1)
+            {
+                string selectedLanguage = (string)picker.ItemsSource[selectedIndex];
+
+                HandleLanguageChange(selectedLanguage);
+            }
+        }
+
+        void HandleLanguageChange(string language)
+        {
+            Preferences.Set("AppLanguage", language);
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Preferences.Set("AppFontSize", e.NewValue);
+        }
     }
 }
